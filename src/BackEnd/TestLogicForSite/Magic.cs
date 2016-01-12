a3475bde75aff35d95badd59fa7eed8866babde0
Artsiom Marozau <artsiom_marozau@epam.com>
2016-01-12 11:43:28 +0300

    first commit

---
+++ src/BackEnd/TestLogicForSite/Magic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLogicForSite
+{
+	// This project can output the Class library as a NuGet Package.
+	// To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+	public class Magic
+	{
+		public Magic()
+		{
+		}
+
+		public bool PositiveTest()
+		{
+			return true;
+		}
+
+		public bool NegativeTest()
+		{
+			return false;
+		}
+
+	}
+}
---
+++ src/BackEnd/TestLogicForSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestLogicForSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("TestLogicForSite")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd9b77f0-2c93-46d6-9ed4-b0049b11f976")]
